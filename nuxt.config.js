0c995976013e71496ab75f4063e5f4beab50096e
femifoye <femi.abdul67@gmail.com>
2020-12-21 19:29:57 +0000

    config fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,8 +6,8 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    titleTemplate: "%s - portfolio",
-    title: "portfolio",
+    titleTemplate: "%s - Frontend Engineer",
+    title: "Femi Abdul",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
